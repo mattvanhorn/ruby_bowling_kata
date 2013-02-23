cb86232c01c17104532a50b1400361a47dbf5253
Matthew Vanhorn <mvanhorn@newrelic.com>
2013-02-22 20:55:33 -0800

    kata 5 green

--- bowling_game.rb
+++ bowling_game.rb
@@ -9,8 +9,14 @@ class BowlingGame
   end
 
   def score
-    @rolls.each do |pins|
-      @score += pins
+    while @rolls.any? do
+      roll1 = @rolls.shift
+      roll2 = @rolls.shift
+      if roll1 + roll2 == 10
+        @score += (10 + @rolls.first)
+      else
+        @score += (roll1 + roll2)
+      end
     end
     @score
   end
