afbf9c2b1a2f9b5e2d53cdd159fc6c8ecc15d0d6
Matthew Vanhorn <mvanhorn@newrelic.com>
2013-02-22 16:52:31 -0800

    kata 3 refactor

--- bowling_game_spec.rb
+++ bowling_game_spec.rb
@@ -4,17 +4,20 @@ require_relative './bowling_game'
 
 describe BowlingGame do
   subject(:game){ BowlingGame.new }
-  it "scores zero for a game with no pins knocked down" do
-    20.times do
-      game.roll(0)
+
+  def roll_many(rolls, pins)
+    rolls.times do
+      game.roll(pins)
     end
+  end
+
+  it "scores zero for a game with no pins knocked down" do
+    roll_many 20, 0
     game.score.should == 0
   end
 
   it "scores 20 for a game with 1 pin knocked down on each roll" do
-    20.times do
-      game.roll(1)
-    end
+    roll_many 20, 1
     game.score.should == 20
   end
 end
