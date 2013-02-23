cf2352eb0854ad39238ea9e241e23937fbb46fed
Matthew Vanhorn <mvanhorn@newrelic.com>
2013-02-22 16:44:51 -0800

    kata 2 red

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,3 +1,9 @@
 class BowlingGame
+  def roll(pins)
 
+  end
+
+  def score
+
+  end
 end
--- bowling_game_spec.rb
+++ bowling_game_spec.rb
@@ -3,7 +3,11 @@ require "rspec"
 require_relative './bowling_game'
 
 describe BowlingGame do
-  it "exists" do
-
+  subject(:game){ BowlingGame.new }
+  it "scores zero for a game with no pins knocked down" do
+    20.times do
+      game.roll(0)
+    end
+    game.score.should == 0
   end
 end
