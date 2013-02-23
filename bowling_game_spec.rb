a9faa95c797708b5ccf43a8ddc28db73bfb99944
Matthew Vanhorn <mvanhorn@newrelic.com>
2013-02-22 16:47:12 -0800

    kata 3 red

--- bowling_game_spec.rb
+++ bowling_game_spec.rb
@@ -10,4 +10,11 @@ describe BowlingGame do
     end
     game.score.should == 0
   end
+
+  it "scores 20 for a game with 1 pin knocked down on each roll" do
+    20.times do
+      game.roll(1)
+    end
+    game.score.should == 20
+  end
 end
